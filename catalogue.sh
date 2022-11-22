f1297d3bb7dc00b4586c2afe6827345410989e8f
mohan <venkyravipati99@gmail.com>
2022-11-22 17:17:13 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -4,12 +4,12 @@ useradd roboshop
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
 rm -rf catalogue
-unzip -o /tmp/catalogue.zip
+unzip  /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
 
-sed -i -e 's/MONGO_DNSNAME/mongodb.agileworld.online/' systemd.conf
+sed -i -e 's/MONGO_DNSNAME/mongodb.agileworld.online/' systemd.service
 
 
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
