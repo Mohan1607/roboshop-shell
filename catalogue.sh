f461d7371f8cd947805f325d024e34e2b308c81c
mohan <venkyravipati99@gmail.com>
2022-11-22 17:06:24 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -8,10 +8,10 @@ mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
 
-#sed -i -e 's/MONGODB_'
+sed -i -e 's/MONGO_DNSNAME/mongodb.agileworld.online/' systemd.conf
 
 
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# systemctl daemon-reload
-# systemctl start catalogue
-# systemctl enable catalogue
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
