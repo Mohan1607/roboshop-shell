ae44dfa42beb020ece1d4cc3562f303626c83ea1
mohan <venkyravipati99@gmail.com>
2022-11-22 17:14:27 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,8 @@ yum install nodejs -y
 useradd roboshop
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
-unzip /tmp/catalogue.zip
+rm -rf catalogue
+unzip -o /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
