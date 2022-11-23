25d299d6f58b05c3e82d98e8ff296aa371f39ea3
mohan <venkyravipati99@gmail.com>
2022-11-23 12:58:02 +0530

    mysql script

---
+++ mysql.sh
@@ -0,0 +1,8 @@
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+dnf module disable mysql
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+DEFAULT_PASSWORD=$(grep 'A temporary passwd' /var/log/mysqld.log | awk '{print $NF}')
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" > /tmp/root-pass-sql
+cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
