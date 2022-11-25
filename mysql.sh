5a8133a95170d5ffebdc6bf4f8c609c4e68cd590
mohan <venkyravipati99@gmail.com>
2022-11-25 18:40:09 +0530

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -1,38 +1,43 @@
-echo -e "\e[34mDOWNLOADING MYSQL REPO\E[0m"
+echo -e "\e[35mDOWNLOADING MYSQL REPO\E[0m"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 if [ $? -eq 0 ] ; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e"\e[31mFAILURE\e[0m"
+    exit
 fi
 
-echo -e "\e[34mDISABLE MYSQL 8\e[0m"
+echo -e "\e[35mDISABLE MYSQL 8\e[0m"
 dnf module disable mysql -y
 if [ $? -eq 0 ] ; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e"\e[31mFAILURE\e[0m"
+    exit
 fi
-echo -e "\e[34mINSTALL MYSQL SERVICE\e[0m"
+echo -e "\e[35mINSTALL MYSQL SERVICE\e[0m"
 yum install mysql-community-server -y
 if [ $? -eq 0 ] ; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e"\e[31mFAILURE\e[0m"
+    exit
 fi
-echo -e "\e[34mENABLE MYSQL SERVICE\e[0m"
+echo -e "\e[35mENABLE MYSQL SERVICE\e[0m"
 systemctl enable mysqld
 if [ $? -eq 0 ] ; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e"\e[31mFAILURE\e[0m"
+    exit
 fi
 systemctl restart mysqld
-echo -e "\e[34mSTART MYSQL SERVICE\e[0m"
+echo -e "\e[35mSTART MYSQL SERVICE\e[0m"
 if [ $? -eq 0 ] ; then
   echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e"\e[31mFAILURE\e[0m"
+     exit
 fi
 echo show databases | mysql -uroot -pRoboshop@1
 if [ $? -ne 0 ]
