e52029508cf90d76c5ed061d42d91d2458a68291
mohan <venkyravipati99@gmail.com>
2022-11-25 18:00:15 +0530

    mysql script

--- mysql.sh
+++ mysql.sh
@@ -3,8 +3,12 @@ dnf module disable mysql -y
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
+echo show databases | mysql -uroot -pRoboshop@1
+if [ $? -ne 0 ]
+then
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 #echo ${DEFAULT_PASSWORD}
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" > /tmp/root-pass-sql
 #ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';
 cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+fi
