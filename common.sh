6a448bae3154ad72a568ec01438b34a5ad4a64f3
mohan <venkyravipati99@gmail.com>
2022-11-29 19:16:58 +0530

    common script

--- common.sh
+++ common.sh
@@ -33,6 +33,22 @@ PRINT "ENABLE ${COMPONENT} SERVICE"
 systemctl enable ${COMPONENT} &>> $LOG
 STAT $?
 }
+SYSTEMD_CONFIG()
+{
+    PRINT "CHANGE ENDPOINT LISTENIP"
+    sed -i -e 's/REDIS_ENDPOINT/redis.agileworld.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.agileworld.online/' -e 's/MONGO_ENDPOINT/mongo.agileworld.online/' -e 's/MONGO_DNSNAME/mongo.agileworld.online/'  /home/roboshop/${COMPONENT}/systemd.service &>> $LOG
+    STAT $?
+    PRINT "CHANGE CONF FILE"
+    mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOG
+    STAT $?
+    PRINT "RELOAD START ENABLE ${COMPONENT} SERVICE"
+    systemctl daemon-reload &>> $LOG && systemctl start ${COMPONENT} &>> $LOG && systemctl enable ${COMPONENT} &>> $LOG
+    STAT $?
+
+}
+
+
+
 NODEJS()
 {
   PRINT "DOWNLOAD NODEJS "
@@ -47,37 +63,37 @@ NODEJS()
   useradd roboshop  &>> $LOG
   fi
   STAT $?
-  PRINT "DOWNLOAD ${COMPONENT} APPLICATION CODE"
-  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> $LOG
-  STAT $?
-  PRINT "REMOVE THE PREVIOUS CONTENT OF ${COMPONENT}"
-  cd /home/roboshop
-  rm -rf ${COMPONENT} &>> $LOG
-  STAT $?
-  PRINT "EXTRACT THE APP CONTENT"
-  unzip -o /tmp/${COMPONENT}.zip &>> $LOG
-  STAT $?
-  #DOWNLOAD_APP_CODE
+  #curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> $LOG
+  #STAT $?
+  #PRINT "REMOVE THE PREVIOUS CONTENT OF ${COMPONENT}"
+  #cd /home/roboshop
+  #rm -rf ${COMPONENT} &>> $LOG
+  #STAT $?
+  #PRINT "EXTRACT THE APP CONTENT"
+  #unzip -o /tmp/${COMPONENT}.zip &>> $LOG
+  #STAT $?
+  DOWNLOAD_APP_CODE
   mv ${COMPONENT}-main ${COMPONENT}
   cd ${COMPONENT}
   PRINT "INSTALL DEPENDENCIES"
   npm install &>> $LOG
   STAT $?
-  PRINT "CHANGE ENDPOINT LISTENIP"
-  sed -i -e 's/REDIS_ENDPOINT/redis.agileworld.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.agileworld.online/' systemd.service &>> $LOG
-  STAT $?
-  PRINT "CHANGE CONF FILE"
-  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOG
-  STAT $?
-  PRINT "LOAD THE SYSTEMD FILE"
-  systemctl daemon-reload &>> $LOG
-  STAT $?
-  PRINT "START ${COMPONENT} SERVICE"
-  systemctl start ${COMPONENT} &>> $LOG
-  STAT $?
-  PRINT "ENABLE ${COMPONENT} SERVICE"
-  systemctl enable ${COMPONENT} &>> $LOG
-  STAT $?
+  SYSTEMD_CONFIG
+  #PRINT "CHANGE ENDPOINT LISTENIP"
+  #sed -i -e 's/REDIS_ENDPOINT/redis.agileworld.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.agileworld.online/' -e 's/MONGO_ENDPOINT/mongo.agileworld.online/' -e 's/MONGO_DNSNAME/mongo.agileworld.online/'  systemd.service &>> $LOG
+  #STAT $?
+  #PRINT "CHANGE CONF FILE"
+  #mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOG
+  #STAT $?
+  #PRINT "LOAD THE SYSTEMD FILE"
+  #systemctl daemon-reload &>> $LOG
+  #STAT $?
+  #PRINT "START ${COMPONENT} SERVICE"
+  #systemctl start ${COMPONENT} &>> $LOG
+  #STAT $?
+  #PRINT "ENABLE ${COMPONENT} SERVICE"
+  #systemctl enable ${COMPONENT} &>> $LOG
+  #STAT $?
 
 }
 
