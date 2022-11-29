ab54c4e08e4500e8b045a0658ad12bf143cbb3be
mohan <venkyravipati99@gmail.com>
2022-11-29 12:49:40 +0530

    cart script

--- cart.sh
+++ cart.sh
@@ -1,33 +1,5 @@
 COMPONENT=cart
 source common.sh
-PRINT "DOWNLOAD NODEJS "
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOG
-STAT $?
-PRINT "INSTALL NODEJS"
-yum install nodejs -y &>> $LOG
-STAT $?
-PRINT "ADD APPLICATION USER"
-id roboshop &>> $LOG
-if [ $? -ne 0 ]; then
-useradd roboshop  &>> $LOG
-fi
-STAT $?
-PRINT "DOWNLOAD APPLICATION CODE"
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> $LOG
-STAT $?
-cd /home/roboshop
-rm -rf ${COMPONENT}
-PRINT "EXTRACT THE APP CONTENT"
-unzip -o /tmp/${COMPONENT}.zip &>> $LOG
-STAT $?
-mv ${COMPONENT}-main ${COMPONENT}
-cd ${COMPONENT}
-PRINT "INSTALL DEPENDENCIES"
-npm install &>> $LOG
-STAT $?
-PRINT "CHANGE ENDPOINT LISTENIP"
-sed -i -e 's/REDIS_ENDPOINT/redis.agileworld.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.agileworld.online/' systemd.service &>> $LOG
-STAT $?
 
 mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 systemctl daemon-reload
--- common.sh
+++ common.sh
@@ -12,4 +12,60 @@ PRINT(){
 }
 LOG=/tmp/$COMPONENT.log
 #rm -f $LOG
+DOWNLOAD_APP_CODE(){
+PRINT "DOWNLOAD APPLICATION CODE"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> $LOG
+STAT $?
+cd /home/roboshop
+rm -rf ${COMPONENT}
+PRINT "EXTRACT THE APP CONTENT"
+unzip -o /tmp/${COMPONENT}.zip &>> $LOG
+STAT $?
+mv ${COMPONENT}-main ${COMPONENT}
+cd ${COMPONENT}
+}
+NODEJS()
+{
+  PRINT "DOWNLOAD NODEJS "
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOG
+  STAT $?
+  PRINT "INSTALL NODEJS"
+  yum install nodejs -y &>> $LOG
+  STAT $?
+  PRINT "ADD APPLICATION USER"
+  id roboshop &>> $LOG
+  if [ $? -ne 0 ]; then
+  useradd roboshop  &>> $LOG
+  fi
+  STAT $?
+  PRINT "DOWNLOAD APPLICATION CODE"
+  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> $LOG
+  STAT $?
+  cd /home/roboshop
+  rm -rf ${COMPONENT}
+  PRINT "EXTRACT THE APP CONTENT"
+  unzip -o /tmp/${COMPONENT}.zip &>> $LOG
+  STAT $?
+  mv ${COMPONENT}-main ${COMPONENT}
+  cd ${COMPONENT}
+  PRINT "INSTALL DEPENDENCIES"
+  npm install &>> $LOG
+  STAT $?
+  PRINT "CHANGE ENDPOINT LISTENIP"
+  sed -i -e 's/REDIS_ENDPOINT/redis.agileworld.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.agileworld.online/' systemd.service &>> $LOG
+  STAT $?
+  PRINT "CHANGE CONF FILE"
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> $LOG
+  STAT $?
+  PRINT "LOAD THE ${COMPONENT} SERVICE"
+  systemctl daemon-reload &>> $LOG
+  STAT $?
+  PRINT "START ${COMPONENT} SERVICE"
+  systemctl start ${COMPONENT} &>> $LOG
+  STAT $?
+  PRINT "ENABLE ${COMPONENT} SERVICE"
+  systemctl enable ${COMPONENT} &>> $LOG
+  STAT $?
+
+}
 
