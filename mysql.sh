ecce921bd2150e49020335c67159c0d821262e7d
mohan <venkyravipati99@gmail.com>
2022-11-30 15:25:19 +0530

    common script

--- mysql.sh
+++ mysql.sh
@@ -1,33 +1,49 @@
+COMPONENT=mysql
 source common.sh
 if [ -z "$1" ]; then
   echo input argument password needed
   exit
 fi
 PRINT "DOWNLOADING MYSQL REPO"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>> $LOG
 STAT $?
 
 PRINT "DISABLE MYSQL 8"
-dnf module disable mysql -y
+dnf module disable mysql -y &>> $LOG
 STAT $?
 PRINT "INSTALL MYSQL SERVICE"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>> $LOG
 STAT $?
 PRINT "ENABLE MYSQL SERVICE"
-systemctl enable mysqld
+systemctl enable mysqld &>> $LOG
 STAT $?
 PRINT "START MYSQL SERVICE"
-systemctl restart mysqld
+systemctl restart mysqld &>> $LOG
 $?
 PRINT "Change Mysql Default Password"
 ROBOSHOP_MYSQL_PASSWORD=$1
-echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
-echo $?
+echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}  &>> $LOG
+echo $? &>> $LOG
 if [ $? -ne 0 ]
 then
-DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}') &>> $LOG
 #echo ${DEFAULT_PASSWORD}
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROBOSHOP_MYSQL_PASSWORD}';" > /tmp/root-pass-sql &>> $LOG
 #ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';
-cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+cat /tmp/root-pass-sql | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" &>> $LOG
 fi
+STAT $?
+PRINT "UNINSTALL VALIDATE PASSWORD PLUGIN"
+echo "show plugins" | mysql -uroot -pRoboshop@1 | grep validate_password &>> $LOG
+if [ $? -eq 0 ]; then
+echo "uninstall plugin validate_password" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>> $LOG
+fi
+STAT $?
+APP_LOC=/tmp
+CONTENT=mysql-main
+DOWNLOAD_APP_CODE
+cd mysql-main &>> $LOG
+PRINT "LOAD SHIPPING SCHEMA"
+mysql -u root -p${ROBOSHOP_MYSQL_PASSWORD} <shipping.sql
+STAT $?
+
