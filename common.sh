21c63e0ff7beca147a9a1da977bd9c284eaeec83
mohan <venkyravipati99@gmail.com>
2022-12-01 13:57:57 +0530

    payment script

--- common.sh
+++ common.sh
@@ -96,21 +96,7 @@ PYTHON(){
   pip3 install -r requirements.txt &>> $LOG
   STAT $?
   exit
-  1. Update the roboshop user and group id in `payment.ini` file.
-  2. Update SystemD service file
+  }
 
-      Update `CARTHOST` with cart server ip
-
-      Update `USERHOST` with user server ip
-
-      Update `AMQPHOST` with RabbitMQ server ip.
-
-  3. Setup the service
-
-  # mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-  # systemctl daemon-reload
-  # systemctl enable payment
-  # systemctl start payment
-}
 
 
