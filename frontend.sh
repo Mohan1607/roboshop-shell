7e574a494c7d0d52c57aaf2eebda857640621f67
mohan <venkyravipati99@gmail.com>
2022-12-06 18:32:08 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -14,6 +14,9 @@ mv frontend-main/static/* .
 PRINT "MOVE THE CONF FILE"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG
 STAT $?
+PRINT "Update RoboShop Configuration"
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.devopsworld.online/'  -e '/user/ s/localhost/dev-user.devopsworld.online/' -e '/cart/ s/localhost/dev-cart.devopsworld.online/' -e '/shipping/ s/localhost/dev-shipping.devopsworld.online/' -e '/payment/ s/localhost/dev-payment.devopsworld.online/' /etc/nginx/default.d/roboshop.conf
+STAT $?
 PRINT "START NGINX SERVICE"
 systemctl restart nginx &>> $LOG
 STAT $?
