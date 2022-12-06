686d0f0f2ba29c9b61a291a30840a36f98eac0df
mohan <venkyravipati99@gmail.com>
2022-12-06 18:52:17 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -5,17 +5,13 @@ source common.sh
 PRINT "INSTALL NGINX SERVICE"
 yum install nginx -y &>> $LOG
 STAT $?
-#curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-#cd /usr/share/nginx/html
-#rm -rf *
-#unzip /tmp/frontend.zip
 DOWNLOAD_APP_CODE
 mv frontend-main/static/* .
 PRINT "MOVE THE CONF FILE"
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG
 STAT $?
 PRINT "Update RoboShop Configuration"
-sed -i -e '/catalogue/ s/localhost/dev-catalogue.devopsworld.online/'  -e '/user/ s/localhost/dev-user.devopsworld.online/' -e '/cart/ s/localhost/dev-cart.devopsworld.online/' -e '/shipping/ s/localhost/dev-shipping.devopsworld.online/' -e '/payment/ s/localhost/dev-payment.devopsworld.online/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/dev-catalogue.agileworld.online/'  -e '/user/ s/localhost/dev-user.agileworld.online/' -e '/cart/ s/localhost/dev-cart.agileworld.online/' -e '/shipping/ s/localhost/dev-shipping.agileworld.online/' -e '/payment/ s/localhost/dev-payment.agileworld.online/' /etc/nginx/default.d/roboshop.conf
 STAT $?
 PRINT "START NGINX SERVICE"
 systemctl restart nginx &>> $LOG
